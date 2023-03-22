9bae98a8644531f43058d2c6f8fb2b1a27f915a7
GAMINSCO <83284620+GAMINSCO@users.noreply.github.com>
2023-03-22 13:45:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 function setup() {
-   canvas = createCanvas(500, 400)
-   canvas.position(600, 180)
+   canvas = createCanvas(500, 400);
+   canvas.position(750, 180);
+
+   video = createCapture(VIDEO);
+   video.size(450, 350,);
+   video.position(150, 200);
+
+   posenet = ml5.poseNet(video, modelLoaded);
+   posenet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+   console.log("Model is Loaded");
+}
+
+function gotPoses(results) {
+   if ( results.length > 0 ) {
+      console.log(results);
+   } else {
+      console.error("error");
+   }
+}
+
+function draw() {
+   background(224, 224, 224);
 }
