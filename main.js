2280fba3c5c036d59df3fb6df839921347c2b6a3
GAMINSCO <83284620+GAMINSCO@users.noreply.github.com>
2023-03-23 20:02:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
+RW = 0;
+LW = 0;
+Dif = 0;
+
+
 function setup() {
    canvas = createCanvas(500, 400);
    canvas.position(750, 180);
@@ -16,7 +22,9 @@ function modelLoaded() {
 
 function gotPoses(results) {
    if ( results.length > 0 ) {
-      console.log(results);
+      LW = results[0].pose.leftWrist.x;
+      RW = results[0].pose.rightWrist.x;
+      Dif = Math.floor(LW - RW);
    } else {
       console.error("error");
    }
@@ -24,4 +32,11 @@ function gotPoses(results) {
 
 function draw() {
    background(224, 224, 224);
+
+   document.getElementById("size").innerHTML = "Font Size = " + Dif + "px";
+
+   fill('#ffd166');
+   text("TEXT", 10, 300);
+
+   textSize(Dif);
 }
